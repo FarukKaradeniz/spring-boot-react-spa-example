f41ae1f80a1253c04bce509080fd7c1c33efc65a
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2019-07-02 15:29:18 +0300

    Initial Commit

---
+++ is-ilani-backend/src/main/java/com/farukkaradeniz/isilanibackend/IsIlaniBackendApplication.java
@@ -0,0 +1,13 @@
+package com.farukkaradeniz.isilanibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsIlaniBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsIlaniBackendApplication.class, args);
+    }
+
+}
