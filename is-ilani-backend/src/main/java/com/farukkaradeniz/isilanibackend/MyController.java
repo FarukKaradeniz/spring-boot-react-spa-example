e1d992431381d73725c8a8a56874633afafb0836
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2019-07-08 11:18:58 +0300

    Working ldap login

---
+++ is-ilani-backend/src/main/java/com/farukkaradeniz/isilanibackend/MyController.java
@@ -0,0 +1,24 @@
+package com.farukkaradeniz.isilanibackend;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping(value = "/logout-success")
+    public String getLogoutPage(Model model){
+        return "logout";
+    }
+
+    @GetMapping(value = "/test")
+    public String testPage(Model model) {
+        return "this is a test response";
+    }
+
+    @GetMapping(value = "/")
+    public String home() {
+        return "home";
+    }
+}
--- is-ilani-frontend/src/App.js
+++ is-ilani-frontend/src/App.js
@@ -1,25 +1,80 @@
 import React from 'react';
 import './App.css';
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={""} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          p React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    api: 'http://localhost:8080',
+    username: '',
+    password: '',
+    response: 'Nothing',
+  };
+
+  onTestClick = () => {
+    Axios({
+      method: 'get',
+      url: `${this.state.api}/connect/linkedIn`
+    }).then(response => console.log(response));
+  };
+
+
+  onInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  };
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    console.log(this.state.username, this.state.password)
+    Axios({
+      method: 'post',
+      url: `${this.state.api}/login`,
+      data: {
+        username: this.state.username,
+        password: this.state.password
+      }
+    }).then(response => console.log(response));
+
+
+
+  };
+
+
+  render() {
+
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={""} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <form onSubmit={this.onSubmitForm}>
+            <input type={"text"} name={"username"} value={this.state.username} onChange={this.onInputChange}/>
+            <input type={"text"} name={"password"} value={this.state.password} onChange={this.onInputChange}/>
+            <input type={"submit"}/>
+          </form>
+
+
+
+          <h2
+            onClick={this.onTestClick}
+            className="App-link"
+            rel="noopener noreferrer"
+          >
+            p React
+          </h2>
+
+          <h3>{this.state.response}</h3>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
