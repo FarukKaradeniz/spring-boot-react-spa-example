a3e15f6e624c787d2ccb72086a594760e9232b36
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2019-07-09 15:27:38 +0300

    frontend test işe yarayan bir kod commiti

--- is-ilani-frontend/src/App.js
+++ is-ilani-frontend/src/App.js
@@ -1,78 +1,50 @@
 import React from 'react';
 import './App.css';
-import Axios from "axios";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 
-class App extends React.Component{
+
+import Home from './Home';
+import Protected from './Protected';
+import Login from './Login';
+import PrivateRoute from './PrivateRoute';
+import Profil from './Profil';
+
+
+class App extends React.Component {
 
   state = {
-    api: 'http://localhost:8080',
-    username: '',
-    password: '',
-    response: 'Nothing',
+    authenticated: false,
   };
 
-  onTestClick = () => {
-    Axios({
-      method: 'get',
-      url: `${this.state.api}/connect/linkedIn`
-    }).then(response => console.log(response));
+  setAuthenticated = () => {
+    if (this.state.authenticated===false){
+      this.setState({authenticated: true})
+    }
   };
-
-
-  onInputChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    })
-  };
-
-  onSubmitForm = (e) => {
-    e.preventDefault();
-    console.log(this.state.username, this.state.password)
-    Axios({
-      method: 'post',
-      url: `${this.state.api}/login`,
-      data: {
-        username: this.state.username,
-        password: this.state.password
-      }
-    }).then(response => console.log(response));
-
-
-
-  };
-
 
   render() {
 
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={""} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <li>
+              <Link to={"/"}>Home</Link>
+              <Link to={"/protected"}>Protected</Link>
+              <Link to={"/profil"}>Profil</Link>
+            </li>
 
-          <form onSubmit={this.onSubmitForm}>
-            <input type={"text"} name={"username"} value={this.state.username} onChange={this.onInputChange}/>
-            <input type={"text"} name={"password"} value={this.state.password} onChange={this.onInputChange}/>
-            <input type={"submit"}/>
-          </form>
+          </header>
 
+          <Route path={"/"} exact component={Home} />
+          <Route path="/login"
+                 render={() => <Login authenticate={this.setAuthenticated} authenticated={this.state.authenticated}/>} />
+          <PrivateRoute path={"/protected"} authenticated={this.state.authenticated} component={Protected}/>
+          <PrivateRoute path={"/profil"} authenticated={this.state.authenticated} component={Profil}/>
 
-
-          <h2
-            onClick={this.onTestClick}
-            className="App-link"
-            rel="noopener noreferrer"
-          >
-            p React
-          </h2>
-
-          <h3>{this.state.response}</h3>
-
-        </header>
-      </div>
+        </div>
+      </Router>
     );
   }
 }
