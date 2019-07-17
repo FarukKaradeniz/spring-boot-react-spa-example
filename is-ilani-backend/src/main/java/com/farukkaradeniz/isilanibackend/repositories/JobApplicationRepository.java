2ae0719d26c513e2e079b934fa56514020a631a1
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2019-07-17 10:15:52 +0300

    jobApplicationRepository updated

--- is-ilani-backend/src/main/java/com/farukkaradeniz/isilanibackend/repositories/JobApplicationRepository.java
+++ is-ilani-backend/src/main/java/com/farukkaradeniz/isilanibackend/repositories/JobApplicationRepository.java
@@ -4,6 +4,12 @@ import com.farukkaradeniz.isilanibackend.models.JobApplication;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JobApplicationRepository extends CrudRepository<JobApplication, String> {
+    List<JobApplication> findAllByCandidate_UserId(String id);
+    List<JobApplication> findAllByCandidate_UserIdAndAndStatus(String id, String status);
+    List<JobApplication> findAllByJobPost_JobPostId(String id);
+    JobApplication findAllByCandidate_UserIdAndAndJobPost_JobPostId(String candidate_id, String jobPost_id);
 }
