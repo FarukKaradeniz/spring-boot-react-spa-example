a570a2143827abe068211d280f12a22e43661285
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2019-07-18 15:21:09 +0300

    Joblist done

---
+++ is-ilani-backend/src/main/java/com/farukkaradeniz/isilanibackend/controllers/ProfileController.java
@@ -0,0 +1,9 @@
+package com.farukkaradeniz.isilanibackend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//@RequestMapping("/api")
+public class ProfileController {
+}
--- is-ilani-frontend/src/JobListItem.js
+++ is-ilani-frontend/src/JobListItem.js
@@ -2,21 +2,21 @@ import React from "react";
 
 import Card from "react-bootstrap/Card";
 import Alert from "react-bootstrap/Alert";
-import moment from "moment";
 
 export default class JobListItem extends React.Component {
   render() {
     return (
       <Card>
         <Alert variant="primary">
-          <b>{this.props.title}</b>
+          <b>YYYY</b>
         </Alert>
         <Card.Body>
           <Card.Text>
-            {this.props.description}
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
           </Card.Text>
           <Card.Subtitle className="text-muted">
-            Posted on {moment(new Date(this.props.deadline)).format("Do MMMM YYYY")}
+            Posted on 2.7.2019
           </Card.Subtitle>
         </Card.Body>
       </Card>
