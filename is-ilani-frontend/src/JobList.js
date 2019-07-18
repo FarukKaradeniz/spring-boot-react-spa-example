19b0722a27c2d3f87d927b971c5a31854090a5eb
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2019-07-18 15:43:58 +0300

    fixed commit mistake

--- is-ilani-frontend/src/JobList.js
+++ is-ilani-frontend/src/JobList.js
@@ -2,44 +2,67 @@ import React from "react";
 import {ListGroup} from "react-bootstrap";
 
 import JobListItem from "./JobListItem";
+import Axios from "axios";
+import {Link} from "react-router-dom";
+
+const baseUrl = "http://localhost:8080";
+const pathUrl = "/api/jobpost";
 
 export default class JobList extends React.Component {
+  
+  state = {
+    posts: [],
+  };
+
+  componentDidMount = () => {
+    this.getJobPosts();
+  };
+
+  populateUI = () => {
+    return this.state.posts.map((data, index) => {
+      return(
+        <div key={index}>
+          <Link to={`/jobdetail/${data.jobPostId}`}>
+            <JobListItem
+              title={data.title}
+              description={data.description}
+              deadline={data.deadline}
+            />
+          </Link>
+          <br />
+        </div>
+      );
+    });
+  };
+  
+  getJobPosts = () => {
+    let jobPostRequest = {
+      url: `${baseUrl}${pathUrl}`,
+      method: 'get',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      params: {
+        isAvailable: true
+      }
+    };
+
+    Axios(jobPostRequest).then(
+      response => {
+        console.log(response);
+        this.setState(
+          {
+            posts: response.data,
+          }
+        );
+      }
+    );
+  };
+
   render() {
     return (
       <ListGroup>
-        <ListGroup.Item className="justify-content-center">
-          <JobListItem />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <JobListItem />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <JobListItem />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <JobListItem />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <JobListItem />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <JobListItem />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <JobListItem />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <JobListItem />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <JobListItem />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <JobListItem />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <JobListItem />
-        </ListGroup.Item>
+        {this.populateUI()}
       </ListGroup>
     );
   }
