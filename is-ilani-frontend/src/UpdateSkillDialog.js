26780015d4d6b8592fcbfe23381c077d56853943
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2019-07-22 20:59:23 +0300

    New information dialog for profile page

---
+++ is-ilani-frontend/src/UpdateSkillDialog.js
@@ -0,0 +1,90 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Axios from "axios";
+
+
+const baseUrl = "http://localhost:8080";
+const pathUrl = "/api/candidate";
+export default class UpdateSkillDialog extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true,
+      skills: props.skills,
+      profileImg: props.profileImg,
+    }
+  }
+
+
+  componentDidMount() {
+    this.setState({skills: this.props.skills});
+  };
+
+  handleClose = () => {
+    this.setState({show: false});
+  };
+
+  onSubmit = () => {
+    this.setState({show: false});
+    let jobPostRequest = {
+      url: `${baseUrl}${pathUrl}`,
+      method: 'put',
+      params: {
+        id: this.props.id
+      },
+      data: {
+        skills: this.state.skills,
+        profileImg: this.state.profileImg,
+      },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+        //todo buralara authorization header'ı eklenecek
+      }
+    };
+
+    Axios(jobPostRequest).then(
+      response => {
+        console.log(response);
+        this.props.updated(response.data.profileImg, response.data.skills);
+      }
+    );
+
+  };
+
+  render() {
+    return (
+      <div>
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Update Skill</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Control
+              value={this.state.skills || ''}
+              onChange={e => this.setState({ skills: e.target.value })}
+              type="text"
+              placeholder="Enter skills"
+            />
+            <br />
+            <Form.Control
+              value={this.state.profileImg || ''}
+              onChange={e => this.setState({ profileImg: e.target.value })}
+              type="text"
+              placeholder="Enter profile image url"
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.onSubmit}>
+              Submit
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+
+
+};
