94186c2d9f497a7d80b9e0ba0856e8e58ed2142a
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2019-07-23 08:42:08 +0300

    link to signup page

--- is-ilani-frontend/src/LoginPage.js
+++ is-ilani-frontend/src/LoginPage.js
@@ -4,6 +4,7 @@ import LoginHr from "./LoginHr";
 import "./LoginPage.css";
 import LoginLinkedin from "./LoginLinkedin";
 import Row from "react-bootstrap/Row";
+import {Link} from "react-router-dom";
 
 export default class LoginPage extends React.Component {
 
@@ -26,6 +27,21 @@ export default class LoginPage extends React.Component {
             authenticated={this.props.authenticated}
             setAuthenticate={this.props.setAuthenticate}
           />
+        </Row>
+        <Row
+          style={{ marginBottom: "10px", marginTop: "40px" }}
+        >
+        </Row>
+        <Row
+          className="text-muted"
+          style={{ marginTop: "40px" }}
+        >
+          If you don't have an account
+        </Row>
+        <Row>
+          <Link to={"/signup"}>
+             SIGN UP
+          </Link>
         </Row>
       </div>
     );
